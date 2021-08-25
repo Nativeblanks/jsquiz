325a3521565d0321ef52e11003aba687229f8074
Zach B <nativeblanks@gmail.com>
2021-08-25 15:55:53 -0400

    add countdown timer

--- script.js
+++ script.js
@@ -1,36 +1,132 @@
 
+// consts defined
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
+// variables 
+let shuffledQuestions, currentQuestionIndex
 
+//  set timer
+var count = 20;
+var interval = setInterval(function(){
+  document.getElementById('timer').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    resetState()
+    document.getElementById('timer').innerHTML='Out of Time';
+  }
+}, 1000);
 
-const startButton = document.getElementById("start-btn");
-const questionContainerElement = document.getElementById("question-container");
-
-startButton.addEventListener("click", startGame);
-
-
+//  Start button action
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+  countdown()
+})
+// start game function
 function startGame() {
-    console.log("started");
-    startButton.classList.add("hide");
-    questionContainerElement.classList.remove("hide");
-    setNextQuestion();
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
-
+// showing and randomizing the question order
 function setNextQuestion() {
-
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-
-
-function selectAnser() {
-
-}
-
-const question = [
-    {
-        question: "what is 2 + 2",
-        answer : [
-            { text: "4", correct: true },
-            { text: "22", correct: false },
-        ]
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
     }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+// to clear out previous information before putting the new data on the screen
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+//  questions and answers available
+const questions = [
+  {
+    question: 'JavaScript and Java are interchangeable',
+    answers: [
+      { text: 'False', correct: true },
+      { text: 'True', correct: false }
+    ]
+  },
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answers: [
+      { text: '<script>', correct: true },
+      { text: '<javascript>', correct: false },
+      { text: '<js>', correct: false },
+      { text: '<scripting>', correct: false }
+    ]
+  },
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    answers: [
+      { text: 'msg("Hello World")', correct: false },
+      { text: 'alertBox("Hello World")', correct: false },
+      { text: 'alert("Hello World")', correct: true },
+      { text: 'msgBox("Hello World")', correct: false }
+    ]
+  },
+  {
+    question: 'How do you call a function named "myFunction"?',
+    answers: [
+      { text: 'call function myFunction()', correct: false },
+      { text: 'myFunction()', correct: true },
+      { text: 'call myfunction()', correct: false },
+      { text: 'oh function myFunction', correct: false }
+    ]
+  }
 ]
